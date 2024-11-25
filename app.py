0ad4fd455be9051e0efe1c5db5b144e892ef8360
rl102306 <2306919780101@ingenieria.usac.edu.gt>
2024-11-25 02:33:41 -0600

    [ADD] Empresa
    Se agrega el modulo de empresa.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from config_app.jwt_config import configure_jwt
-from routes import auth_routes , cliente_routes,suscripcion_routes,dashboard_routes
+from routes import auth_routes , cliente_routes,suscripcion_routes,dashboard_routes,empresa_routes
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +11,7 @@ def create_app():
     app.register_blueprint(cliente_routes.cliente_bp, url_prefix='/api/cliente')  
     app.register_blueprint(suscripcion_routes.suscripcion_bp,url_prefix='/api/suscripcion')
     app.register_blueprint(dashboard_routes.dashboard_bp,url_prefix='/api/dashboard')
+    app.register_blueprint(empresa_routes.empresa_bp,url_prefix='/api/empresa')
     return app
 
 if __name__ == '__main__':
